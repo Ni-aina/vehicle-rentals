b49d6d996bd24e7faece5647e076416de087a3e5
Ni-aina <niaina.dev.soft@gmail.com>
2025-04-08 13:53:14 +0300

    Header Modal

--- components/vehicles/category/Header.tsx
+++ components/vehicles/category/Header.tsx
@@ -10,19 +10,21 @@ export const VehiclesCategoryHeader = () => {
     return (
         <div className="flex justify-between items-center w-full gap-3">
             <div className="flex justify-between items-center gap-3">
-                <Modal>
-                    <Modal.Open opens="category-form">
-                        <button
-                            className="p-4 h-11 border rounded-md border-border 
-                    flex items-center justify-center font-bold hover:bg-border"
-                        >
-                            +
-                        </button>
-                    </Modal.Open>
-                    <Modal.Window name="category-form" title="Category">
-                        <VehiclesCategoryForm id={null} />
-                    </Modal.Window>
-                </Modal>
+                <form action="">
+                    <Modal>
+                        <Modal.Open opens="category-form">
+                            <button
+                                className="p-4 h-11 border rounded-md border-border 
+                        flex items-center justify-center font-bold hover:bg-border"
+                            >
+                                +
+                            </button>
+                        </Modal.Open>
+                        <Modal.Window name="category-form" title="Category">
+                            <VehiclesCategoryForm id={null} />
+                        </Modal.Window>
+                    </Modal>
+                </form>
                 <h1 className="text-lg">Add category</h1>
             </div>
             <div className="flex justify-between gap-3">
